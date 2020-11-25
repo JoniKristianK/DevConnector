dfb8feabdf337e03fc64a9f53300cf6283d392d8
JoniKristianK <joni.karilainen@gmail.com>
2020-11-25 21:42:55 +0200

    Delete Profile Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -152,7 +152,7 @@ router.delete('/', auth, async (req, res) => {
 
 // @route   PUT api/profile/experience
 // @dec     Add profile experience
-// access   Public
+// access   Private
 router.put(
   '/experience',
   [
@@ -204,4 +204,26 @@ router.put(
   }
 );
 
+// @route   DELETE api/profile/experience/:exp_id
+// @dec     Delete experience from profile
+// access   Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
