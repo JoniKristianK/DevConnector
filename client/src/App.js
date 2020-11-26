717bef2be1b3d35abb5a1dcda8dceeef98c3807b
JoniKristianK <joni.karilainen@gmail.com>
2020-11-26 19:20:54 +0200

    Alert Reducer, Action & Types

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
@@ -8,18 +12,20 @@ import Login from './components/auth/Login';
 import './App.css';
 
 const App = () => (
-  <Router>
-    <>
-      <NavBar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </section>
-    </>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <>
+        <NavBar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </>
+    </Router>
+  </Provider>
 );
 
 export default App;
