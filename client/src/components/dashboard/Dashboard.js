de565dbdcd5943307f30d240f04d1e91e9cb243e
JoniKristianK <joni.karilainen@gmail.com>
2020-12-02 18:46:34 +0200

    List Education & Experience

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import Spinner from '../layout/Spinner';
+import Experience from '../dashboard/Experience';
+import Education from '../dashboard/Education';
 import DashboardActions from './DashboardActions';
 
 import { getCurrentProfile } from '../../actions/profile';
@@ -29,6 +31,8 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment>
           <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
         </Fragment>
       ) : (
         <Fragment>
