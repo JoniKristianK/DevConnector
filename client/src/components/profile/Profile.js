cefa6fe8626c506d51223a130399f51e7083041d
JoniKristianK <joni.karilainen@gmail.com>
2020-12-07 10:01:55 +0200

    Profile Experience & Education Diplay

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,8 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -37,6 +39,30 @@ const Profile = ({
           <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
+            <div className='profile-exp bg-white p-2'>
+              <div className='h2'>Experience</div>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map((exp) => (
+                    <ProfileExperience key={exp._id} experience={exp} />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No ecperience credentials</h4>
+              )}
+            </div>
+            <div className='profile-edu bg-white p-2'>
+              <div className='h2'>Education</div>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map((edu) => (
+                    <ProfileEducation key={edu._id} education={edu} />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education credentials</h4>
+              )}
+            </div>
           </div>
         </Fragment>
       )}
---
+++ client/src/components/profile/ProfileEducation.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+
+const ProfileEducation = ({
+  education: {
+    school,
+    degree,
+    location,
+    fieldofstudy,
+    current,
+    to,
+    from,
+    description,
+  },
+}) => (
+  <div>
+    <div className='h3 text-dark'>{school}</div>
+    <p>
+      <Moment format='YYYY/MM/DD'>{from}</Moment> -{' '}
+      {!to ? 'Now' : <Moment format='YYYY/MM/DD'>{to}</Moment>}
+    </p>
+    <p>
+      <strong>Degree: </strong> {degree}
+    </p>
+    <p>
+      <strong>Field Of Study: </strong> {fieldofstudy}
+    </p>
+    <p>
+      <strong>Description: </strong> {description}
+    </p>
+  </div>
+);
+
+ProfileEducation.propTypes = {
+  education: PropTypes.array.isRequired,
+};
+
+export default ProfileEducation;
